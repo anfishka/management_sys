fa044361a93ff9962f2e9fc59180e17263676a38
Hanna Borodina <venera131916@gmail.com>
2023-11-29 23:51:45 +0200

    add test data

--- card.js
+++ card.js
@@ -1,12 +1,14 @@
-fetch('products_db.json')
+document.addEventListener('DOMContentLoaded', () => {
+   fetch('/products.json')
 .then(response => response.json())
 .then(data => {
-    const product = data.products[0].classification.hooks;
+    const hooks = data.products[0].classification.hooks;
+    const product = hooks[0];
     document.getElementById('product-image').src = product.img;
     document.getElementById('product-name').textContent = product.name;
     document.getElementById('product-price').textContent =`Price: ${product.sellingPrice}` ;
-    document.getElementById('product-image').src = product.img;
-    document.getElementById('product-image').src = product.img;
-    
+  
 })
-.catch(error => console.error('Error fetching data:', error));
+.catch(error => console.log('Error fetching data:', error)); 
+});
+
